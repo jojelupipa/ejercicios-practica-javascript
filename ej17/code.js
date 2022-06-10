3a764207d503ce1b10dfdbaa2d4679f41d4f7f3a
Jesús Sánchez <jesus_38b@hotmail.com>
2022-06-10 09:30:28 +0200

    Added code for ej17

---
+++ ej17/code.js
@@ -0,0 +1,9 @@
+
+// Button callback definition
+$('#azul').mouseup(function () { 
+    $('#texto').css('color', 'blue');;
+});
+
+$('#rojo').mouseup(function () { 
+    $('#texto').css('color', 'red');;
+});
