d6378a4835b81f4bfc8bfec80acb3de0751f1b3f
Jesús Sánchez <jesus_38b@hotmail.com>
2022-06-10 11:44:28 +0200

    Added code for ej19

---
+++ ej19/code.js
@@ -0,0 +1,7 @@
+
+$('img').mouseenter(function () { 
+    $(this).attr('src', 'https://github.com/DavidBernalGonzalez/EjerciciosJQuery/blob/main/Ejercicio%20carta/card-front.png?raw=true');
+});
+$('img').mouseleave(function () { 
+    $(this).attr('src', 'https://github.com/DavidBernalGonzalez/EjerciciosJQuery/blob/main/Ejercicio%20carta/card-back.png?raw=true');
+});
